70433bf3a2e8f57ee77ed749d38ce6e8c708bc55
Magesh Kuppan <tkmagesh77@gmail.com>
2024-07-29 15:25:51 +0530

    function variables

---
+++ 06-functions/04-fn-vars.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// 0 arguments, 0 return values
+	var sayHi func()
+	sayHi = func() {
+		fmt.Println("Hi!")
+	}
+	sayHi()
+
+	// 1 argument, 0 return values
+	var greet func(string)
+	greet = func(userName string) {
+		fmt.Printf("Hi %s, Have a nice day!\n", userName)
+	}
+	greet("Magesh")
+
+	// 1 argument, 1 return value
+	var greetUser func(string) string
+	greetUser = func(userName string) string {
+		return fmt.Sprintf("Hi %s, Have a good day!", userName)
+	}
+	msg := greetUser("Suresh")
+	fmt.Println(msg)
+	/*
+		// 2 arguments, 1 return value
+		result := func(x, y int) int {
+			return x + y
+		}(100, 200)
+		fmt.Println("add result :", result)
+
+		// 2 arguments, 2 return values
+		// using named result(s)
+		quotient, remainder := func(x, y int) (quotient, remainder int) { // quotient & remainder are declared and initialized with the zero values of int
+			quotient, remainder = x/y, x%y
+			return
+		}(100, 7)
+		fmt.Printf("q = %d and r = %d\n", quotient, remainder)
+	*/
+
+}
