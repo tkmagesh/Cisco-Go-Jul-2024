801a911b3c35bf3caefc7a69d63bb7ec77642b82
Magesh Kuppan <tkmagesh77@gmail.com>
2024-07-29 16:25:41 +0530

    more on pointers

--- 07-pointers/01-example.go
+++ 07-pointers/01-example.go
@@ -16,4 +16,24 @@ func main() {
 	no = 200
 	x = *noPtr
 	fmt.Println(x)
+
+	no = 999
+	fmt.Println("Before incrementing, no =", no)
+	fmt.Println("&no =", &no)
+	increment(&no)
+	fmt.Println("After incrementing, no =", no)
+
+	n1, n2 := 100, 200
+	fmt.Printf("Before swapping n1 = %d and n2 = %d\n", n1, n2)
+	swap( /* ? */ )
+	fmt.Printf("After swapping n1 = %d and n2 = %d\n", n1, n2)
+}
+
+func increment(val *int) {
+	fmt.Println("[increment] &val = ", val)
+	*val++
+}
+
+func swap( /*  */ ) /* no return values */ {
+	/* swap the given 2 values */
 }
