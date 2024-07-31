e09bdc661996c258840c8dcf2ba3c3314661bffe
Magesh Kuppan <tkmagesh77@gmail.com>
2024-07-31 12:39:17 +0530

    assignment solution

--- 18-concurrency/04-assignment.go
+++ 18-concurrency/04-assignment.go
@@ -4,13 +4,35 @@ Accept a range from the user and print all the prime numbers between the given r
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+var primes []int
+var mutex sync.Mutex
 
 func main() {
 	var start, end int
 	fmt.Println("Enter the range :")
 	fmt.Scanln(&start, &end)
 	// check each number if it is a prime CONCURRENTLY and print the prime numbers
+	wg := &sync.WaitGroup{}
+	for i := start; i <= end; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if isPrime(i) {
+				mutex.Lock()
+				{
+					primes = append(primes, i)
+				}
+				mutex.Unlock()
+			}
+		}()
+	}
+	wg.Wait()
+	fmt.Println(primes)
 }
 
 func isPrime(no int) bool {
