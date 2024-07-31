92a379fc73fdde15da8e753b44905d3f46fdc99f
Magesh Kuppan <tkmagesh77@gmail.com>
2024-07-31 14:28:33 +0530

    more on channels

--- 18-concurrency/05-communication/15-example.go
+++ 18-concurrency/05-communication/15-example.go
@@ -2,27 +2,21 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
+// consumer
 func main() {
-	/*
-		var ch chan int
-		ch = make(chan int)
-	*/
+
 	ch := make(chan int)
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	go add(wg, 100, 200, ch)
+	go add(100, 200, ch)
 	result := <-ch
-	wg.Wait()
 	fmt.Println(result)
 }
 
-func add(wg *sync.WaitGroup, x, y int, ch chan int) {
+// producer
+func add(x, y int, ch chan int) {
 	result := x + y
 	ch <- result
-	wg.Done()
 }
 
 /*
